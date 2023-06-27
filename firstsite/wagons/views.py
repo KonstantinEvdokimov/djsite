b30801b22b0855c066c2aad199647bb1f343dbb1
KonstantinEvdokimov <evdkonst@gmail.com>
2023-06-27 12:20:00 +0300

    Lesson 6. Adding templates. Creating a basic template and its extensions. Displaying data from the database.

--- firstsite/wagons/views.py
+++ firstsite/wagons/views.py
@@ -1,26 +1,30 @@
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render, redirect
 
+from .models import *
+
+menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+posts = Wagons.objects.all()
 
 def index(request):
-    """Возвращает экземпляр объекта HttpResponse.
+    return render(request, 'wagons/index.html', {'posts': posts,'menu': menu, 'title': 'Главная страница'})
 
-    Этот экземпляр будет автоматически формировать нужный заголовок ответа,
-    а содержимое HTML-страницы будет определяться указанной строкой.
-    Параметр request – это ссылка на экземпляр класса HttpRequest,
-    который содержит информацию о запросе, о сессии, о куках и так далее.
-    То есть, через переменную request нам доступна вся возможная информация в рамках текущего запроса."""
-    return HttpResponse("Страница приложения wagons.")
+
+def about(request):
+    return render(request, 'wagons/about.html', {'menu': menu, 'title': 'О сайте'})
+
 
 def categories(request, typeid):
     return HttpResponse(f"<h1>Статьи по категориям</h1>{typeid}</p>")
+
 
 def archive(request, year):
     if (int(year) > 2022):
         # raise Http404()
         # return redirect('/') # Временный редирект 302
-        return redirect('home', permanent=True)# Постоянный редирект 301
+        return redirect('home', permanent=True)  # Постоянный редирект 301
     return HttpResponse(f"<h1>Архив по годам</h1>{year}</p>")
+
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Жаль! Страница не найдена</h1>')
