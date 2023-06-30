7385dbd471b8607eeaadbd4af110c5e08a5b3e0d
KonstantinEvdokimov <evdkonst@gmail.com>
2023-06-30 19:42:28 +0300

    Lesson 10. Setting up the admin panel. Adding the WagonsAdmin and CategoryAdmin classes.

--- firstsite/wagons/apps.py
+++ firstsite/wagons/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class WagonsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'wagons'
+    verbose_name = 'Вагоны'
--- firstsite/wagons/models.py
+++ firstsite/wagons/models.py
@@ -3,13 +3,13 @@ from django.urls import reverse
 
 
 class Wagons(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField(blank=True)
-    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+    title = models.CharField(max_length=255, verbose_name='Заголовок')
+    content = models.TextField(blank=True, verbose_name='Текст статьи')
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name='Изображение')
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    time_update = models.DateTimeField(auto_now=True, verbose_name='Время обновления')
+    is_published = models.BooleanField(default=True, verbose_name='Публикация')
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')
 
     def __str__(self):
         return self.title
@@ -17,12 +17,24 @@ class Wagons(models.Model):
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_id': self.pk})
 
+    class Meta:
+        verbose_name = 'Вагоны железнодорожные' # Отображение в админ-панели
+        verbose_name_plural = 'Вагоны железнодорожные' # То же во множественном числе
+        ordering = ['time_create', 'title'] # Порядок вывода в админ-панели и на сайте
+
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
+    name = models.CharField(max_length=100, db_index=True, verbose_name='Категория')
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
         return reverse('category', kwargs={'cat_id': self.pk})
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['id']
+
+
